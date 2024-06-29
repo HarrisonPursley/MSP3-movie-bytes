6239f18de6c6a376d1c7a634132076831f3308f2
Zeus <purslh1@unlv.nevada.edu>
2024-06-29 00:20:57 -0700

    0.0.5 added health check for GET and POST routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 import express from 'express';
+import morgan from 'morgan';
+import healthCheckRouter from './routes/health-check.js';
 
-
+// === Vars ===
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 
+// === Middlewares ===
+app.use(express.static('public'));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// === Routes ===
+app.use('/src/routes/health-check.js', healthCheckRouter)
+
+
+/**
+ * @method GET
+ * @path /docs
+ */
+app.get('/docs', (req, res) => {
+    res.send('docs...')
+})
+
+
+// === Catch-All Route ===
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>Page Not Found</h1>')
+})
+
+// === Errors ===
 
 
 
